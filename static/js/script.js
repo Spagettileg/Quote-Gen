b0371cfc99d68617a1473a3631eff650257cd2e4
EC2 Default User <ec2-user@ip-172-31-46-140.eu-west-1.compute.internal>
2021-04-22 11:56:35 +0000

    const variables added for quoteContainer, quoteText, authorText, twitterBtn and newQuoteBtn. All variables will link to function in html file.

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,13 @@
+// constant variables - unable to be re-assigned 
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Global Variable
 // let apiQuotes = []; // empty array allows for json data to load into space.
+               
 
 // Show New Quote
 function newQuote() {
@@ -8,7 +16,8 @@ function newQuote() {
     // Math.random will select single random quote from array
     // Can replace apiQuotes with localQuotes, in case of external data fail
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 
 // // Get Quotes from API
@@ -31,4 +40,4 @@ function newQuote() {
 
 // Upon succcessful load of API data
 // getQuotes();
-   newQuote(); 
+   newQuote();
