e92deba4d43f3ca3ea82ed9cb16bc13d12f3a5a8
EC2 Default User <ec2-user@ip-172-31-46-140.eu-west-1.compute.internal>
2021-04-22 11:02:37 +0000

    localQuote function added to show contingency in place of an external api database failure.

--- static/js/script.js
+++ static/js/script.js
@@ -1,32 +1,34 @@
 // Global Variable
-let apiQuotes = []; // empty array allows for json data to load into space.
+// let apiQuotes = []; // empty array allows for json data to load into space.
 
 // Show New Quote
 function newQuote() {
     // Select a random quote from apiQuotes array
     // Math.floor will return rounded whole number
     // Math.random will select single random quote from array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Can replace apiQuotes with localQuotes, in case of external data fail
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
     console.log(quote);
 }
 
-// Get Quotes from API
-async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
+// // Get Quotes from API
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes';
     
-    // try to collect API data. If not working, then catch error to solve & fix  
+//     // try to collect API data. If not working, then catch error to solve & fix  
     
-    try {
-      const response = await fetch(apiUrl); 
-      // response const created when we have API data, else error message
-      apiQuotes = await response.json();
-      // console.log(apiQuotes); used to view appearance of API data in console
-      newQuote();
-    } catch (error) {
-        alert('Unable to load data...');
-        // Catch error here
-    }
-}
+//     try {
+//       const response = await fetch(apiUrl); 
+//       // response const created when we have API data, else error message
+//       apiQuotes = await response.json();
+//       // console.log(apiQuotes); used to view appearance of API data in console
+//       newQuote();
+//     } catch (error) {
+//         alert('Unable to load data...');
+//         // Catch error here
+//     }
+// }
 
 // Upon succcessful load of API data
-getQuotes();
+// getQuotes();
+   newQuote(); 
