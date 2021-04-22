84ee68687d1038602f3f6837801b5680ba666f46
EC2 Default User <ec2-user@ip-172-31-46-140.eu-west-1.compute.internal>
2021-04-22 12:17:58 +0000

    If statement updated to replace an erroneous special character.

--- static/js/script.js
+++ static/js/script.js
@@ -24,7 +24,7 @@ function newQuote() {
     }
     
     // Check Quote length to determine styling
-    if (quote.text,length > 50) {
+    if (quote.text.length > 50) {
         quoteText.classList.add('long-quote');
     } else {
         quoteText.classList.remove('long-quote');
